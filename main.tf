5ebf0f7ad7d4093ccef3488ba31d63ef9895da9e
aza <armin.zaugg@stud.hslu.ch>
2020-07-12 01:12:06 +0200

    test

--- main.tf
+++ main.tf
@@ -2,37 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "prefix-resources"
+resource "azurerm_resource_group" "tf_main_test" {
+  name     = "tf_resource"
   location = "West Europe"
-}
-
-resource "azurerm_app_service_plan" "main" {
-  name                = "prefix-asp"
-  location            = "West Europe"
-  resource_group_name = "tf_test"
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-}
-
-resource "azurerm_app_service" "main" {
-  name                = "prefix-appservice"
-  location            = "West Europe"
-  resource_group_name = "tf_test"
-  app_service_plan_id = "someid"
-
-  site_config {
-    app_command_line = ""
-    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
-  }
-
-  app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
-  }
 }
